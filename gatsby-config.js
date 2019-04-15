3fb3d3b6a466176822d18d46d00aed7fcd1d837c
Edison Ade <ask@buzzedison.com>
2019-04-15 10:43:28 +0100

    change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `SouledOut website`,
+    description: `We are all about Jesus. Expressing the Kingdom of God on earth`,
+    author: `@souledoutlive`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
